2959ec21e11d488e62d15e851910a0e689b6aa7c
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-07-24 20:25:48 +0300

    Added check script

--- check/check.php
+++ check/check.php
@@ -8,16 +8,28 @@ function check_site($url) {
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  $response = curl_exec($ch);
+
+  $result = curl_exec($ch);
+
   $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
   curl_close($ch);
   return $http_code;
 }
 
-$email = 'strange-humans-4@sw.consulting';
 $http_code = check_site($site_url);
-$message = $site_url . ' response ' . $http_code;
+if ($http_code == 200) {
+  $subject = 'Proxy-c service OK';
+}
+else {
+  $subject = 'Proxy-c service ERROR';
+}
 
-mail($email, $subject, $message);
+$current_time = date('H:i');
 
+if ($http_code != 200 || ($current_time > '10:30' && $current_time < '11:30')) {
+    $email = 'strange-humans-4@sw.consulting';
+    $message = $site_url . ' response ' . strval($http_code);
+    mail($email, $subject, $message);
+//    echo $message;
+}
 ?>
